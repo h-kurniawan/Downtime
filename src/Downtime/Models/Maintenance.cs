ba932c5e5fbb5d3d39d898fb6e8b0c0834f575e4
Herry Kurniawan <hkurniawan@mdsol.com>
2019-03-08 16:53:43 +0900

    Removed DataContract and DataMember attributes

--- src/Downtime/Models/Maintenance.cs
+++ src/Downtime/Models/Maintenance.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Downtime.Models
 {
-    [DataContract]
     public class Maintenance
     {
-        [DataMember(Name = "start")]
         public string Start { get; set; }
 
-        [DataMember(Name = "end")]
         public string End { get; set; }
 
-        [DataMember(Name = "product_name")]
         public string ProductName { get; set; }
 
-        [DataMember(Name = "products")]
         public IEnumerable<ProductUrl> ProductUrls { get; set; }
 
-        [DataMember(Name = "draft_message")]
         public string DraftMessage { get; set; }
     }
 }
--- src/Downtime/Models/ProductUrl.cs
+++ src/Downtime/Models/ProductUrl.cs
@@ -1,24 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Threading.Tasks;
 
 namespace Downtime.Models
 {
-    [DataContract]
     public class ProductUrl
     {
-        [DataMember(Name = "url")]
         public Uri Url { get; set; }
 
-        [DataMember(Name = "iso_language")]
         public string IsoLanguage { get; set; }
 
-        [DataMember(Name = "project_managers")]
         public IEnumerable<string> ProjectManagers { get; set; }
 
-        [DataMember(Name = "email_contacts")]
         public IEnumerable<string> EmailContacts { get; set; }
     }
 }
