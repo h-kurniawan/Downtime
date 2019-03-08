458a5cbd9994007ebe1ca344007ccc58741f0ab1
Herry Kurniawan <hkurniawan@mdsol.com>
2019-03-08 15:59:12 +0900

    Modified resource response

--- src/Downtime/Controllers/DowntimesController.cs
+++ src/Downtime/Controllers/DowntimesController.cs
@@ -10,37 +10,39 @@ namespace Downtime.Controllers
     public class DowntimesController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Index()
+        [Route("{id:int}")]
+        public IActionResult Index(int id)
         {
             var productUrls = new ProductUrl[] {
                 new ProductUrl
                 {
                     Url = new Uri("https://amgen.mdsol.co.jp"),
                     IsoLanguage = "ja",
+                    ProjectManagers = new[] { "John Doe" },
+                    EmailContacts = new[] { "john.doe@amgen.co.jp" }
                 },
                 new ProductUrl
                 {
                     Url = new Uri("https://amgen.mdsol.co.kr"),
                     IsoLanguage = "kr",
+                    ProjectManagers = new[] { "John Wick" },
+                    EmailContacts = new[] { "john.wick@amgen.co.kr" }
                 },
                 new ProductUrl
                 {
                     Url = new Uri("https://amgen.mdsol.co.ch"),
                     IsoLanguage = "zh",
+                    ProjectManagers = new[] { "Jack Reacher" },
+                    EmailContacts = new[] { "jack.reacher@amgen.co.zh" }
                 }
-            };
-
-            var products = new Product
-            {
-                Name = "Rave EDC",
-                ProductUrls = productUrls
             };
 
             var maintenance = new Maintenance
             {
                 Start = DateTimeOffset.Now.AddDays(7).To8601(),
                 End = DateTimeOffset.Now.AddDays(8).To8601(),
-                Products = new[] { products },
+                ProductName = "Rave EDC",
+                ProductUrls = productUrls,
                 DraftMessage = ""
             };
 
