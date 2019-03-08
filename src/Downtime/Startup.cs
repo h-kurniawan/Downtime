47acadd55eabb2ab912797924e6ec3c8f061ffa5
Herry Kurniawan <hkurniawan@mdsol.com>
2019-03-08 14:41:55 +0900

    Fix failed docker build

--- src/Downtime/Startup.cs
+++ src/Downtime/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 
 namespace Downtime
 {
@@ -24,7 +25,23 @@ namespace Downtime
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services
+                .AddRouting(options => options.LowercaseUrls = true)
+                .AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters()
+                .AddDataAnnotations()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new SnakeCaseNamingStrategy()
+                        {
+                            ProcessDictionaryKeys = true
+                        }
+                    };
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
